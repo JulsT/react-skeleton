119bc4931c578ba8378d94b28b8b0f927a3cde57
Yuliia Tarasiuk <yuliia.tarasiuk@swanteams.com>
2022-02-04 17:35:56 +0200

    add primereact, reduxtoolkit

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-import { override, addWebpackAlias } from 'customize-cra';
-
-const path = require('path');
-
-module.exports = override(addWebpackAlias({
-  '@': path.resolve(__dirname, './src'),
-  '@const': path.resolve(__dirname, './src/utils/const'),
-  '@helpers': path.resolve(__dirname, './src/utils/helpers'),
-}));
